2e3311417dac28cb4e557c2966f377d1084ca99c
Raz Yaron <raz.yaron@post.runi.ac.il>
2023-05-20 17:08:46 +0300

    Second_Commit

--- setup.sh
+++ setup.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
 
-# Update system packages
-sudo apt-get update
+# Set variables
+AMI="ami-024e6efaf93d85776"  # Updated AMI ID
+INSTANCE_TYPE="t2.micro"
+SECURITY_GROUP_NAME="my-security-group"
+KEY_PAIR_NAME="my-key-pair"
+VOLUME_SIZE=16
+VOLUME_TYPE="gp3"
 
-# Install necessary packages
-sudo apt-get install -y python3-pip
+# Create security group
+aws ec2 create-security-group --group-name $SECURITY_GROUP_NAME --description "My security group"
+aws ec2 authorize-security-group-ingress --group-name $SECURITY_GROUP_NAME --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-# Install Python dependencies
-sudo pip3 install flask
+# Launch EC2 instance
+INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-groups $SECURITY_GROUP_NAME --key-name $KEY_PAIR_NAME --block-device-mappings DeviceName=/dev/sda1,Ebs={VolumeSize=$VOLUME_SIZE,VolumeType=$VOLUME_TYPE} | grep "InstanceId" | awk -F'"' '{print $4}')
 
-# Copy application files to appropriate directories
-sudo cp /path/to/application.py /var/www/
+# Wait for instance to be running
+aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-# Set up service or run the application
-# ...
+# Get public IP address of the instance
+PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 
-# Additional setup steps
-# ...
+# Copy code to the instance
+scp -i ~/.ssh/my-key-pair.pem parking_lot.py ubuntu@$PUBLIC_IP:/home/ubuntu/
 
-# Display instructions for running the application
-echo "Setup completed successfully."
-echo "To start the application, execute the following command:"
-echo "python3 parking_lot.py"
+# Connect to the instance and execute commands
+ssh -i ~/.ssh/my-key-pair.pem ubuntu@$PUBLIC_IP << EOF
+    # Install dependencies (assuming Ubuntu)
+    sudo apt-get update
+    sudo apt-get install -y python3 python3-pip
+
+    # Install Flask
+    pip3 install flask
+
+    # Run the Python code
+    python3 /home/ubuntu/parking_lot.py
+EOF
+
+# Print instructions for executing the code
+echo "The code has been deployed to the EC2 instance."
+echo "To access the instance, use the following command:"
+echo "ssh -i ~/.ssh/my-key-pair.pem ubuntu@$PUBLIC_IP"
+echo "Once connected to the instance, you can run the code by executing the following command:"
+echo "python3 /home/ubuntu/parking_lot.py"
