fc3dec89a0d83f92e8246f9364671426fd1d4885
Raz Yaron <raz.yaron@post.runi.ac.il>
2023-05-21 22:09:54 +0300

    WorkingSetup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-KEY_NAME="Raz-Lotem-HW1-`date +'%N'`"
+# KEY_NAME="Raz-Lotem-HW1-Test-$(date +'%N')"
+KEY_NAME="Raz-Lotem-HW1-Test-4"
 KEY_PEM="$KEY_NAME.pem"
 
 echo "create key pair $KEY_PEM to connect to instances and save locally"
@@ -10,7 +11,8 @@ aws ec2 create-key-pair --key-name $KEY_NAME \
 # secure the key pair
 chmod 400 $KEY_PEM
 
-SEC_GRP="Raz-Lotem-sg-`date +'%N'`"
+# SEC_GRP="Raz-Lotem-secG-$(date +'%N')"
+SEC_GRP="Raz-Lotem-secG4"
 
 echo "setup firewall $SEC_GRP"
 aws ec2 create-security-group   \
@@ -32,45 +34,36 @@ aws ec2 authorize-security-group-ingress        \
     --group-name $SEC_GRP --port 5000 --protocol tcp \
     --cidr $MY_IP/32
 
-UBUNTU_22_04_AMI="ami-053b0d53c279acc90"
+UBUNTU_22_04_AMI="ami-01dd271720c1ba44f"
 
 echo "Creating Ubuntu 22.04 instance..."
 RUN_INSTANCES=$(aws ec2 run-instances   \
     --image-id $UBUNTU_22_04_AMI        \
     --instance-type t2.micro            \
     --key-name $KEY_NAME                \
-    --security-groups $SEC_GRP         \
-    --block-device-mappings '[
-        {
-            "DeviceName": "/dev/xvda",
-            "Ebs": {
-                "VolumeSize": 16,
-                "VolumeType": "gp3"
-            }
-        }
-    ]'
+    --security-groups $SEC_GRP         #\
 )
 
-INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[1].InstanceId')
+INSTANCE_ID=$(echo $RUN_INSTANCES | jq -r '.Instances[0].InstanceId')
 
 echo "Waiting for instance creation..."
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
 PUBLIC_IP=$(aws ec2 describe-instances  --instance-ids $INSTANCE_ID | 
-    jq -r '.Reservations[0].Instances[1].PublicIpAddress'
+    jq -r '.Reservations[0].Instances[0].PublicIpAddress'
 )
 
 echo "New instance $INSTANCE_ID @ $PUBLIC_IP"
 
 echo "deploying code to production"
-scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" app.py ubuntu@$PUBLIC_IP:/home/ubuntu/
+scp -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=60" parking_lot.py ubuntu@$PUBLIC_IP:/home/ubuntu/
 
 # Connect to the instance and execute commands
 echo "setup production environment"
 ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP <<EOF
     # Install dependencies (assuming Ubuntu)
     sudo apt-get update
-    sudo apt-get install jq
+
     sudo apt-get install -y python3 python3-pip
 
     # Install Flask
